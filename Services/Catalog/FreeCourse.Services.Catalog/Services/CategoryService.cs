efe1d995da2988e9e7c9fd43fb872d66e83bb596
fehminqurbanli <fehminqurbanli2@gmail.com>
2022-08-24 16:58:35 +0400

    adding controllers

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog.Services
 {
-    internal class CategoryService:ICategoryService
+    public class CategoryService:ICategoryService
     {
         private readonly IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
@@ -30,11 +30,12 @@ namespace FreeCourse.Services.Catalog.Services
             return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
         }
 
-        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        public async Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto)
         {
+            var category=_mapper.Map<Category>(categoryDto);
             await _categoryCollection.InsertOneAsync(category);
 
-            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category),200);
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(categoryDto),200);
         }
 
         public async Task<Response<CategoryDto>> GetByIdAsync(string id)
